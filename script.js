37c8df7f3f3eb910bf07b6059b8395b4920ac8dc
ni2dey <neetudey500@gmail.com>
2025-06-23 00:59:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function togglemenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+const boxes = document.querySelectorAll('.description');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+boxes.forEach(box => observer.observe(box));
